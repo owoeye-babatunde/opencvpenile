7fb393031bcfc706292d82dd9909c9b213dcafff
Devopdevs <owoeyebabatunde01@gmail.com>
2024-02-02 23:53:01 -0800

    handle possible length error

--- cvapp.py
+++ cvapp.py
@@ -19,5 +19,20 @@ if image:
         #manipulated_image = enhance_image(image, brightness)
     length, width = measure_length_width(image)
 
-st.write(f"The length is {round(length, 2)} cm")
-st.write(f"The width is {round(width, 2)} cm")
+if length >= 24:
+    st.write(f"We are about 99% confident that the detected length {round(length, 2)} cm is statistically invalid. \
+              Kindly take another picture, keeping a reasonable distance between you and your penis. \
+              For maximum accuracy, make sure that a significant portion of the image is penis only")
+    st.write(f"The detected width is {round(width, 2)} cm")
+
+elif length > 18:
+    st.write(f"Are you sure that this image is not doctored or more than 1X zoomed? the detected length is {round(length, 2)} cm")
+    st.write(f"The detected width is {round(width, 2)} cm")
+
+
+else:
+    st.write(f"The length is {round(length, 2)} cm")
+    st.write(f"The width is {round(width, 2)} cm")
+
+
+
